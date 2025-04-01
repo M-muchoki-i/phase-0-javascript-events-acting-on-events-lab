250ff3711e7bea9aa8835dfdcfdcc33eb3c83280
michelle <michellemuchoki8@gmail.com>
2025-04-01 20:13:35 +0300

    events

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Your code here
+function moveDodgerLeft() {
+  const left = parseInt(dodger.style.left);
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+}
+
+function moveDodgerRight() {
+  const left = parseInt(dodger.style.left);
+  if (left + 40 < 400) {
+    dodger.style.left = `${left + 1}px`;
+  }
+}
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "ArrowLeft") {
+    moveDodgerLeft();
+  } else if (e.key === "ArrowRight") {
+    moveDodgerRight();
+  }
+});
+
